56d4100b63cb872592c5e140a9e10e1bd5690187
Alfons Wallin <144wallfin@gmail.com>
2025-10-28 16:18:18 +0100

    Wiped everything; restarting from scratch (Ran into some trouble)

--- preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('darkMode', {
-  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
-  system: () => ipcRenderer.invoke('dark-mode:system')
-  // we can also expose variables, not just functions
-})
